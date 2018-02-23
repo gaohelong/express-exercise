97f7eb5956d87b99228c5c3d81631e7e910a46d1
helong.gao <helong.gao@sinobbd.com>
2018-02-23 16:21:07 +0800

    express-exercise: base

---
+++ node-example/index.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/china', function (req, res) {
+    res.send('China!');
+});
+
+let server = app.listen(3000, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
