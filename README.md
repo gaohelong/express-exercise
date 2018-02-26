28ee1b66ec186ecd281309b8548ae7e18f92f5dd
helong.gao <helong.gao@sinobbd.com>
2018-02-26 15:42:02 +0800

    express-exercise: 托管静态文件

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Express
 [官方手册](https://expressjs.com/) [中文手册](http://www.expressjs.com.cn/)
+
+### jade模版引擎
+[手册](http://jade-lang.com)
--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // 访问地址->http://localhost:3000/images/avatar.png.
+// app.use('/static', express.static(path.join(__dirname, 'public'))); // 指定一个虚拟目录, 访问地址->http://localhost:3000/static/images/avatar.png.
 
 app.use('/', index);
 app.use('/users', users);
